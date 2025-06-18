39486e6570f9390b580f56fe7f9d19b5007fb32e
Azhar Saleh <azhar@Azhars-MacBook-Pro.local>
2025-06-18 12:32:12 -0700

    addded lab2

---
+++ lab2.py
@@ -0,0 +1,23 @@
+# Lab 1 
+
+## Step 1
+name = "Azhar S"
+
+## Step 2
+nickname = "Rose"
+
+## Step 3 
+age = 14
+
+## Step 4 
+used_python = "False"
+
+## Step 5
+hobbies = ["reading", "playing games", "netflix", "cooking"]
+
+## Step 6
+favorite_things = {"show": "Dexter",'Food' : "fresh bread"}
+
+print (name)
+print(nickname)
+print(age)  
