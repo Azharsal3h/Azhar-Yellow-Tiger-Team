0f08f131626d6318cb9ed88d8f42d1612ca11310
Azharsal3h <azharsaleh857@gmail.com>
2025-06-28 14:43:54 -0700

    Add files via upload

---
+++ lab3.py
@@ -0,0 +1,23 @@
+# Lab 3
+
+# Task 1: Working with Strings 
+food = 'Pizza'
+print(food[0:3]) #print first 3 characters
+print(food[-3:-1]) #print last 3 characters
+first_last = food[0] + food[-1] #combine first and last character
+print(first_last)
+food_list = food.split()
+print(food_list) 
+joined_food = ' '.join(food_list)
+print(joined_food) 
+
+# task 2: working with lists 
+number_list = [1, 394383, 32, -5, 0, 234]
+number_list.append(67)
+#print(number_list)
+number_list.insert(3, 1000)
+#print(number_list) 
+number_list.pop()
+#print(number_list) 
+number_list.remove(number_list[1])
+
